b1d34f7494109c2f6d0febce3f3e0dcb944ace17
YangLabHKUST <66421162+YangLabHKUST@users.noreply.github.com>
2021-08-25 21:14:47 +0800

    Update MRAPSS_EM_funcs.R

--- R/MRAPSS_EM_funcs.R
+++ R/MRAPSS_EM_funcs.R
@@ -1,10 +1,10 @@
 # The function for calculating likelihood
 cal_likeli <- function(hat.b, A, Var, S, C1, C2, pi0){
 
-  Sigma <- mapply(function(M1,M2){M1 +  A %*% M2 %*% t(A) }, M1=S, M2=Var)
-  Sigma = lapply(split(Sigma, col(Sigma)), function(x){matrix(x, nrow=2, ncol=2)})
+  S2 <- mapply(function(M1,M2){M1 +  A %*% M2 %*% t(A) }, M1=S, M2=Var)
+  S2 = lapply(split(S2, col(S2)), function(x){matrix(x, nrow=2, ncol=2)})
 
-  P1 =  mapply(mvtnorm::dmvnorm, x= hat.b,  sigma=Sigma)
+  P1 =  mapply(mvtnorm::dmvnorm, x= hat.b,  sigma=S2)
   P2 =  mapply(mvtnorm::dmvnorm, x= hat.b,  sigma=S)
 
   #return likelihood
