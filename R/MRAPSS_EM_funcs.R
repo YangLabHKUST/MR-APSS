ddb918905fe3ffedab46f38454c72cec725643b9
YangLabHKUST <66421162+YangLabHKUST@users.noreply.github.com>
2021-08-25 22:13:50 +0800

    Add files via upload

--- R/MRAPSS_EM_funcs.R
+++ R/MRAPSS_EM_funcs.R
@@ -1,17 +1,3 @@
-# The function for calculating likelihood
-cal_likeli <- function(hat.b, A, Var, S, C1, C2, pi0){
-
-  S2 <- mapply(function(M1,M2){M1 +  A %*% M2 %*% t(A) }, M1=S, M2=Var)
-  S2 = lapply(split(S2, col(S2)), function(x){matrix(x, nrow=2, ncol=2)})
-
-  P1 =  mapply(mvtnorm::dmvnorm, x= hat.b,  sigma=S2)
-  P2 =  mapply(mvtnorm::dmvnorm, x= hat.b,  sigma=S)
-
-  #return likelihood
-  return(sum(log(pi0 * P1/2/pnorm(C1) + (1-pi0) * P2/2/pnorm(C2))))
-
-}
-
 # The function for calculating evidence lower bound
 cal_elbo <- function(A, pi0, m, Pi, S, inv.S, Var, inv.Var, muj, Sigmaj, hat.b, C1, C2){
 
--- R/MR_EM_fixr_funcs.R
+++ R/MR_EM_fixr_funcs.R
@@ -1,19 +1,6 @@
-# The function for calculating likelihood
-cal_likeli <- function(hat.b, A, Var, S, C1, C2, pi0){
+#' @export
 
-  S2 <- mapply(function(M1,M2){M1 +  A %*% M2 %*% t(A) }, M1=S, M2=Var)
-  S2 = lapply(split(S2, col(S2)), function(x){matrix(x, nrow=2, ncol=2)})
-
-  P1 =  mapply(mvtnorm::dmvnorm, x= hat.b,  sigma=S2)
-  P2 =  mapply(mvtnorm::dmvnorm, x= hat.b,  sigma=S)
-
-  #return likelihood
-  return(sum(log(pi0 * P1/2/pnorm(C1) + (1-pi0) * P2/2/pnorm(C2))))
-
-}
-
-
-MRAPSS_EM_fixr_func <- function(data = NULL,
+MR_EM_fixr_func <- function(data = NULL,
                                 beta =NULL,
                                 Sigma = NULL,
                                 pi0 = NULL,
@@ -54,8 +41,6 @@ MRAPSS_EM_fixr_func <- function(data = NULL,
   # genome wide shared + s_j(\rho)
   S = matrix(s + Omega, nrow=m, ncol=4)
   S11 = S[,1]
-
-
 
   R = matrix(c(1, r, r, 1), nrow = 2, ncol = 2)
   H = t(chol(R))
